8fbbff9dca61073a6212b0a9c92ce4de64a8c25a
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-02-21 21:08:10 +0500

    sd

--- src/server.js
+++ src/server.js
@@ -1,16 +1,38 @@
 import express from "express";
+import listEndpoints from "express-list-endpoints";
 import cors from "cors";
-import profileRoutes from "./services/profiles/index.js";
 import mongoose from "mongoose";
+import {
+  badRequestHandler,
+  unauthorizedHandler,
+  notFoundHandler,
+  genericErrorHandler,
+} from "./errorHandlers.js";
+import expereincesRouter from "./services/experience/model.js";
+import profileRouter from "./services/profiles/index";
+import postRouter from "./services/postmode/index";
+
 const server = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3001;
+
 server.use(cors());
 server.use(express.json());
-server.use("/profiles",profileRoutes)
- mongoose.connect(process.env.MONGO_CONNECTION);
- mongoose.connection.on("connected", () => {
-      console.log("connected to mongo");
+
+server.use("/profile", expereincesRouter);
+server.use("/profiles", profileRouter);
+server.use("/postMode", postRouter);
+
+server.use(badRequestHandler);
+server.use(unauthorizedHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
+
+mongoose.connect(process.env.MONGO_CONNECTION);
+
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to Mongo!");
   server.listen(port, () => {
-    console.log("server is running on port", port);
+    console.table(listEndpoints(server));
+    console.log("Server runnning on port: ", port);
   });
- });
+});
