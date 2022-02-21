032efeff5f907baf44d57f7aeb1229c68af00d1e
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-02-21 14:27:49 +0500

    updated

--- src/services/experience/model.js
+++ src/services/experience/model.js
@@ -4,43 +4,39 @@ import createHttpError from "http-errors";
 
 const expereincesRouter = express.Router();
 
-expereincesRouter.post("/", async(req, res, next) => {
-    try {
-        const newExperience = new ExperiencesModel(req.body);
+expereincesRouter.post("/", async (req, res, next) => {
+  try {
+    const newExperience = new ExperiencesModel(req.body);
     const { _id } = await newExperience.save();
     res.status(201).send(newExperience);
-    } catch (error) {
-        next(error)
-    }
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
-expereincesRouter.get("/", async(req, res, next) => {
-    try {
-        
-    } catch (error) {
-        next(error)
-    }
-})
-expereincesRouter.get("/:userName", async(req, res, next) => {
-    try {
-        
-    } catch (error) {
-        next(error)
-    }
-})
-expereincesRouter.put("/:userName", async(req, res, next) => {
-    try {
-        
-    } catch (error) {
-        next(error)
-    }
-})
-expereincesRouter.delete("/:userName", async(req, res, next) => {
-    try {
-        
-    } catch (error) {
-        next(error)
-    }
-})
+expereincesRouter.get("/", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+expereincesRouter.get("/:userName", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+expereincesRouter.put("/:userName", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+expereincesRouter.delete("/:userName", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
 
-export default expereincesRouter
+export default expereincesRouter;
