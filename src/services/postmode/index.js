f2c1328af80bc908ec42bb668de0295330cc7626
PiotrWachowiakpl <piotrwachowiak81@gmail.com>
2022-02-21 12:01:28 +0100

    basic crud

--- src/services/postmode/index.js
+++ src/services/postmode/index.js
@@ -23,11 +23,7 @@
 
 //     Add an image to the post under the name of "post"
 
- 
-
 //     #EXTRA: Find a way to return also the user with the posts, in order to have the Name / Picture to show it correcly on the frontend
-
- 
 
 // #EXTRA: Make unique endpoints for posting (BOTH text and image together)
 
@@ -37,9 +33,9 @@ import PostModel from "./schema.js";
 import q2m from "query-to-mongo";
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import { v2 as cloudinary} from "cloudinary"
+import { v2 as cloudinary } from "cloudinary";
 
-const postRouter = express.Router()
+const postRouter = express.Router();
 
 // const cloudinaryUplpoad = multer({
 //     storage: new CloudinaryStorage({
@@ -51,15 +47,81 @@ const postRouter = express.Router()
 // }).single("image")
 
 postRouter.post("/", async (req, res, next) => {
-    try {
-      const newPost = new PostModel(req.body);
-      await newPost.save();
-      res.status(201).send({ newPost });
-    } catch (error) {
-      next(error);
+  try {
+    const newPost = new PostModel(req.body);
+    await newPost.save();
+    res.status(201).send({ newPost });
+  } catch (error) {
+    next(error);
+  }
+});
+
+postRouter.get("/", async (req, res, next) => {
+  try {
+    const defaultQuery = {
+      sort: "price",
+      skip: 0,
+      limit: 15,
+    };
+    const query = { ...defaultQuery, ...req.query };
+    const mongoQeury = q2m(query);
+    const total = await PostModel.countDocuments(mongoQeury.criteria);
+
+    const posts = await PostModel.find(mongoQeury.criteria)
+      .sort(mongoQeury.options.sort)
+      .skip(mongoQeury.options.skip)
+      .limit(mongoQeury.limit);
+    res.status(200).send({
+      links: mongoQeury.links("/posts", total),
+      total,
+      titalPagess: Math.ceil(total / mongoQeury.options.limit),
+      posts,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+postRouter.get("/:postId", async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+    const foundPost = await PostModel.findById(postId);
+    if (foundPost) {
+      res.send(foundPost);
+    } else {
+      next(createHttpError(404), `post with given ${postId} not found`);
     }
-  });
+  } catch (error) {
+    next(error);
+  }
+});
 
+postRouter.put("/:postId", async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+    const updatedPost = await PostModel.findByIdAndUpdate(postId, req.body, {
+      new: true,
+    });
+    if (updatedPost) {
+      res.send(updatedPost);
+    } else {
+      next(createHttpError(404, `post with given ${postId} not found`));
+    }
+  } catch (error) {
+    next(createHttpError(500, `internal server problem`));
+  }
+});
+postRouter.delete("/:postId", async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+    const deletedPost = await PostModel.findByIdAndDelete(postId);
+    if (deletedPost) {
+      res.status(204).send();
+    } else {
+      next(createHttpError(404, `post with given ${postId} not found`));
+    }
+  } catch (error) {
+    next(createHttpError(500, "internal server problem"));
+  }
+});
 
-
-  export default postRouter
+export default postRouter;
