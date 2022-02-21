ee31f8c1d9d9610abb173f4a7dec900e7ff108b8
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-02-21 15:46:58 +0500

    image uploading

--- src/services/experience/model.js
+++ src/services/experience/model.js
@@ -1,6 +1,18 @@
 import express from "express";
 import ExperiencesModel from "./schema.js";
 import createHttpError from "http-errors";
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
+const cloudinaryUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: "experience",
+    },
+  }),
+}).single("image");
 
 const expereincesRouter = express.Router();
 
@@ -22,6 +34,7 @@ expereincesRouter.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
 expereincesRouter.get("/:experienceId", async (req, res, next) => {
   try {
     const product = await ExperiencesModel.findById(req.params.experienceId);
@@ -55,6 +68,7 @@ expereincesRouter.put("/:experienceId", async (req, res, next) => {
     next(error);
   }
 });
+
 expereincesRouter.delete("/:experienceId", async (req, res, next) => {
   try {
     const deleted = await ExperiencesModel.findByIdAndDelete(
@@ -71,5 +85,31 @@ expereincesRouter.delete("/:experienceId", async (req, res, next) => {
     next(error);
   }
 });
+
+expereincesRouter.post(
+  "/:experienceId/image",
+  cloudinaryUploader,
+  async (req, res, next) => {
+    try {
+      const experienceId = req.params.experienceId;
+      const updated = await ExperiencesModel.findByIdAndUpdate(
+        experienceId,
+        { image: req.file.path },
+        {
+          new: true,
+        }
+      );
+      if (updated) {
+        res.send(updated);
+      } else {
+        next(
+          createHttpError(404, `Experience with id ${experienceId} not found!`)
+        );
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default expereincesRouter;
