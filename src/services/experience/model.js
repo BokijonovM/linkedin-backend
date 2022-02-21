8a4aeb6a284773b8a118bf115c6cf6153963feb8
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-02-21 15:39:09 +0500

    CRUD is done

--- src/services/experience/model.js
+++ src/services/experience/model.js
@@ -16,24 +16,57 @@ expereincesRouter.post("/", async (req, res, next) => {
 
 expereincesRouter.get("/", async (req, res, next) => {
   try {
+    const product = await ExperiencesModel.find();
+    res.send(product);
   } catch (error) {
     next(error);
   }
 });
-expereincesRouter.get("/:userName", async (req, res, next) => {
+expereincesRouter.get("/:experienceId", async (req, res, next) => {
   try {
+    const product = await ExperiencesModel.findById(req.params.experienceId);
+    if (product) {
+      res.send(product);
+    } else {
+      res
+        .status(404)
+        .send(`Product with id ${req.params.experienceId} not found!`);
+    }
   } catch (error) {
     next(error);
   }
 });
-expereincesRouter.put("/:userName", async (req, res, next) => {
+
+expereincesRouter.put("/:experienceId", async (req, res, next) => {
   try {
+    const updated = await ExperiencesModel.findByIdAndUpdate(
+      req.params.experienceId,
+      req.body,
+      { new: true }
+    );
+    if (updated) {
+      res.send(updated);
+    } else {
+      res
+        .status(404)
+        .send(`Expereince with id ${req.params.experienceId} not found!`);
+    }
   } catch (error) {
     next(error);
   }
 });
-expereincesRouter.delete("/:userName", async (req, res, next) => {
+expereincesRouter.delete("/:experienceId", async (req, res, next) => {
   try {
+    const deleted = await ExperiencesModel.findByIdAndDelete(
+      req.params.experienceId
+    );
+    if (deleted) {
+      res.send("Experience deleted!");
+    } else {
+      res
+        .status(404)
+        .send(`Experience with id ${req.params.experienceId} not found!`);
+    }
   } catch (error) {
     next(error);
   }
