069a412fb7e630611bd3dc6ca6d9bc1bd184e578
hilina88 <hilinawuhib@gmail.com>
2022-02-21 14:48:03 +0100

    added some changes

--- src/services/posts/index.js
+++ src/services/posts/index.js
@@ -1,49 +1,45 @@
 import express from "express";
 import createHttpError from "http-errors";
-import ProfileModel from "./schema.js";
+import ProfilesModel from "./schema.js";
 
 const profileRouter = express.Router();
 
 profileRouter.get("/", async (req, res, next) => {
   try {
-    const profile = await new ProfileModel.find();
+    const profile = await ProfilesModel.find();
     res.send(profile);
-  } catch (error) {
-    next(error);
-  }
-});
-profileRouter.get("/:profileId", async (req, res, next) => {
-  try {
-    const profileId = req.params.profileId;
-    const profile = await ProfileModel.findById(profileId);
-    if (profile) {
-      res.send(profile);
-    } else {
-      next(createHttpError(404, `profile with id ${profileId} not found!`));
-    }
   } catch (error) {
     next(error);
   }
 });
 profileRouter.post("/", async (req, res, next) => {
   try {
-    const newProfile = new ProfileModel(req.body);
+    const newProfile = new ProfilesModel(req.body);
     const { _id } = await newProfile.save();
     res.status(201).send({ _id });
   } catch (error) {
     next(error);
   }
 });
-profileRouter.put("/:profileId", async (req, res, next) => {
+profileRouter.get("/:id", async (req, res, next) => {
   try {
-    const profileId = req.params.profileId;
-    const updatedProfile = await ProfileModel.findByIdAndUpdate(
-      profileId,
-      req.body,
-      {
-        new: true,
-      }
-    );
+    const id = req.params.id;
+    const profile = await ProfilesModel.findById(id);
+    if (profile) {
+      res.send(profile);
+    } else {
+      next(createHttpError(404, `profile with id ${id} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+profileRouter.put("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const updatedProfile = await ProfilesModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
     if (updatedProfile) {
       res.send(updatedProfile);
     } else {
@@ -53,17 +49,5 @@ profileRouter.put("/:profileId", async (req, res, next) => {
     next(error);
   }
 });
-profileRouter.delete("/:profileId", async (req, res, next) => {
-  try {
-    const profileId = req.params.profileId;
-    const deletedProfile = await ProfileModel.findByIdAndDelete(profileId);
-    if (deletedProfile) {
-      res.status(204).send();
-    } else {
-      next(createHttpError(404, `profile with id ${profileId} not found!`));
-    }
-  } catch (error) {
-    next(error);
-  }
-});
+
 export default profileRouter;
--- src/services/posts/schema.js
+++ src/services/posts/schema.js
@@ -2,19 +2,18 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 const profileSchema = new Schema(
   {
-    name: { type: String, required: true },
-    surname: { type: String, required: true },
+    firstName: { type: String, required: true },
+    surName: { type: String, required: true },
     email: {
       type: String,
       required: true,
       match: /.+\@.+\..+/,
-      unique: true,
     },
     bio: { type: String, required: true },
     title: { type: String, required: true },
     area: { type: String, required: true },
-    image: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
+    image: { type: String, required:false },
+    userName: { type: String, required: true },
   },
   {
     timestamps: true,
