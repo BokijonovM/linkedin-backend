8a2e3ee3e99c9b4846923e1ea74dc31c9da6294b
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-02-21 14:03:14 +0500

    updated

--- src/services/experience/model.js
+++ src/services/experience/model.js
@@ -4,4 +4,43 @@ import createHttpError from "http-errors";
 
 const expereincesRouter = express.Router();
 
+expereincesRouter.post("/", async(req, res, next) => {
+    try {
+        const newExperience = new ExperiencesModel(req.body);
+    const { _id } = await newExperience.save();
+    res.status(201).send(newExperience);
+    } catch (error) {
+        next(error)
+    }
+})
+
+expereincesRouter.get("/", async(req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+})
+expereincesRouter.get("/:userName", async(req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+})
+expereincesRouter.put("/:userName", async(req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+})
+expereincesRouter.delete("/:userName", async(req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+})
+
 export default expereincesRouter
