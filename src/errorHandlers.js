b82f080872100440e1805b8d04a253db7b0e8e86
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-02-21 20:28:08 +0500

    updated

--- src/errorHandlers.js
+++ src/errorHandlers.js
@@ -28,22 +28,11 @@ export const notFoundHandler = (err, req, res, next) => {
 export const genericErrorHandler = (err, req, res, next) => {
   console.log("I am the error handler here is the error: ", err);
   res.status(500).send({ message: "Generic Server Error!" });
+  s;
+  if (err.status === 400) {
+    console.log("I am the bad request handler", err);
+    res.status(400).send({ message: err.message, errorsList: err.errorsList });
+  } else {
+    next(err);
+  }
 };
-    if (err.status === 400) {
-      console.log("I am the bad request handler", err)
-      res.status(400).send({ message: err.message, errorsList: err.errorsList })
-    } else {
-      next(err)
-    }
-  }
-  
-  export const unauthorizedHandler = (err, req, res, next) => {
-    if (err.status === 401) {
-      console.log("I am the unauthorized handler", err)
-      res.status(401).send({ message: err.message })
-    } else {
-      next(err)
-    }
-  }
-  
- 
