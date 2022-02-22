d10556c905e611d05918631afd2fc2ec44b0bd18
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-02-22 10:10:12 +0500

    csv downloading is working

--- src/services/profiles/index.js
+++ src/services/profiles/index.js
@@ -1,6 +1,24 @@
 import express from "express";
 import createHttpError from "http-errors";
 import ProfilesModel from "./schema.js";
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import json2csv from "json2csv";
+import { join } from "path";
+import fs from "fs-extra";
+import { pipeline } from "stream";
+
+const { createReadStream, writeJSON } = fs;
+
+const cloudinaryUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: "experience",
+    },
+  }),
+}).single("image");
 
 const profileRouter = express.Router();
 
@@ -169,5 +187,72 @@ profileRouter.delete("/:id/experiences/:expId", async (req, res, next) => {
     );
   }
 });
+
+profileRouter.get("/:id/experiences/:_id/CSV", async (req, res, next) => {
+  try {
+    const _id = req.params.id;
+    const profile = await ProfilesModel.findById(_id);
+    const experienceJSONPath = join(process.cwd(), `./src/data/${_id}.csv`);
+    // console.log(experienceJSONPath);
+    await writeJSON(experienceJSONPath, profile.experiences);
+
+    const filename = `${_id}.csv`;
+    res.setHeader("Content-Disposition", `attachment; filename=${filename}`);
+    const source = createReadStream(experienceJSONPath);
+    const transform = new json2csv.Transform({
+      fields: [
+        "role",
+        "company",
+        "startDate",
+        "endDate",
+        "description",
+        "area",
+        "username",
+        "image",
+      ],
+    });
+    const destination = res;
+
+    pipeline(source, transform, destination, (err) => {
+      if (err) {
+        console.log("Error!!!!");
+      }
+    });
+
+    // res.status(200).send()
+  } catch (error) {
+    next(
+      createHttpError(400, "Some errors occurred in request body!", {
+        message: error.message,
+      })
+    );
+  }
+});
+
+profileRouter.post(
+  "/:id/experiences/:_id/image",
+  cloudinaryUploader,
+  async (req, res, next) => {
+    try {
+      const experienceId = req.params._id;
+      const updated = await ProfilesModel.findByIdAndUpdate(
+        experienceId,
+        { image: req.file.path },
+        {
+          new: true,
+        }
+      );
+      if (updated) {
+        res.send(updated);
+      } else {
+        next(
+          createHttpError(404, `Experience with id ${experienceId} not found!`)
+        );
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default profileRouter;
